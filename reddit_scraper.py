8241b9fedeaa964d6b768a906f43d912203c7213
shane <avila.shane@yahoo.com>
2019-04-13 02:53:49 -0700

    Implemented preliminary way of sending tables through message

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -7,10 +7,16 @@ import praw
 
 posts = []
 title_set = set()
-subreddit = praw.Reddit('user').subreddit('friends').new(limit=25)
+reddit = praw.Reddit('user')
+subreddit = reddit.subreddit('friends').new(limit=25)
 # To avoid posts in multiple subreddits with identical names
 for post in subreddit:
     if post.title not in title_set:
         title_set.add(post.title)
         posts.append([post.title, post.subreddit, post.shortlink, post.num_comments])
 posts = pandas.DataFrame(posts, columns=['title', 'subreddit', 'url', '# of comments'])
+reddit.redditor('urmom723').message('TEST',
+                                    '''Column A | Column B | Column C
+                                    ---------|----------|----------
+                                    A1 | B1 | C1
+                                    A2 | B2 | C2''')
