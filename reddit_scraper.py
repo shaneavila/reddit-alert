edc57a621e3faae3c6b34408dc19df8b1c4ef840
shane <avila.shane@yahoo.com>
2019-04-15 14:03:54 -0700

    Implemented better detection duplicate posts

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,17 +1,21 @@
 import praw
 
 posts = []
-title_set = set()
+url_set = set()
+message_user = 'urmom723'
 reddit = praw.Reddit('user')
 subreddit = reddit.subreddit('friends').new(limit=25)
-# To avoid posts in multiple subreddits with identical names
+# Avoids posts that link to the same url in multiple subreddits
 for post in subreddit:
-    if post.title not in title_set:
-        title_set.add(post.title)
+    if post.url not in url_set:
+        url_set.add(post.url)
         posts.append([post.title, str(post.subreddit), post.shortlink + " ", str(post.num_comments)])
 format_titles = '|'.join(['title', 'subreddit', 'url', '# of comments\n'])
 format_separation = '|'.join(['-', '-', '-', '-\n'])
 for i in range(len(posts)):
     posts[i] = '|'.join(posts[i])
 format_table = '\n'.join(posts)
-reddit.redditor('urmom723').message('New Posts', format_titles + format_separation + format_table)
+reddit.redditor(message_user).message('New Posts', format_titles + format_separation + format_table)
+
+# TODO messaging a username to the bot will add it to friends list
+# TODO keep track of most recent post - if post is same as last post send no message
