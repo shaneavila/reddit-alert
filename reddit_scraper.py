e54a95667011418f9924f729641840263724abe3
shane <avila.shane@yahoo.com>
2019-04-17 21:30:34 -0700

    Implemented scheduling to send message of posts on interval

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,21 +1,30 @@
 import praw
+import schedule
 
 posts = []
 url_set = set()
 message_user = 'urmom723'
 reddit = praw.Reddit('user')
-subreddit = reddit.subreddit('friends').new(limit=25)
-# Avoids posts that link to the same url in multiple subreddits
-for post in subreddit:
+subreddit = reddit.subreddit('friends')
+
+
+# Output post data in markdown table format
+def output():
+    format_title = '|'.join(['title', 'author', 'subreddit', 'url', '# of comments\n'])
+    format_columns = '|'.join(['-', '-', '-', '-', '-\n'])
+    for i in range(len(posts)):
+        posts[i] = '|'.join([posts[i].title, str(posts[i].author), str(posts[i].subreddit),
+                             posts[i].shortlink, str(posts[i].num_comments)])
+    format_table = '\n'.join(posts)
+    reddit.redditor(message_user).message('New Posts', format_title + format_columns + format_table)
+    posts.clear()
+
+
+schedule.every(8).hours.do(output)
+for post in subreddit.stream.submissions(skip_existing=True):
+    # Avoids posts that link to the same url in multiple subreddits
     if post.url not in url_set:
         url_set.add(post.url)
-        posts.append([post.title, str(post.subreddit), post.shortlink + " ", str(post.num_comments)])
-format_titles = '|'.join(['title', 'subreddit', 'url', '# of comments\n'])
-format_separation = '|'.join(['-', '-', '-', '-\n'])
-for i in range(len(posts)):
-    posts[i] = '|'.join(posts[i])
-format_table = '\n'.join(posts)
-reddit.redditor(message_user).message('New Posts', format_titles + format_separation + format_table)
-
-# TODO messaging a username to the bot will add it to friends list
-# TODO keep track of most recent post - if post is same as last post send no message
+        posts.insert(0, post)
+    if len(posts) > 0:
+        schedule.run_pending()
